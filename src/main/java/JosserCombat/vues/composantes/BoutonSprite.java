3a533b768067a436b4beed3847d3847a4b7d19ed
jfs39 <jfs.serg@hotmail.com>
2020-02-14 09:34:37 -0500

    Made BoutonSprite fully funcitonnal

--- src/main/java/JosserCombat/vues/composantes/BoutonSprite.java
+++ src/main/java/JosserCombat/vues/composantes/BoutonSprite.java
@@ -15,35 +15,66 @@ import javafx.scene.paint.Color;
 public class BoutonSprite extends HBox {
 
 	private ImageView imageView;
-	private String url="";
-	//private String url = "C:\\Users\\Jean-François\\SecondWorkSpace\\JosserCombat\\src\\main\\resources\\img\\prototypeJoseph.png";
+	private String url = "/img/prototypeJoseph.png";
+	
+	
+
+	
+	
 	public BoutonSprite(@NamedArg("texte") String texte) {
 		super();
 		J.appel(this);
 		
-		
 		Button bouton = new Button(texte);
+		
+		
+	
+		
+		
 		if (texte.equals("Confirmer")) {
-			this.setAlignment(Pos.BOTTOM_CENTER);
-			bouton.getStyleClass().add("boutonConfirmer");
+			
+			genererBouton(bouton, "boutonConfirmer");
+			
 		} else {
-			this.setAlignment(Pos.BOTTOM_CENTER);
-			bouton.getStyleClass().add("boutonAnnuler");
+			
+			genererBouton(bouton,"boutonAnnuler");
 		}
-		InputStream streamImage = ImageAjustable.class.getResourceAsStream(url);
 		
-		Image image = new Image(streamImage);
-		
-		imageView = new ImageView(image);
-		
-		this.getChildren().add(bouton);
-		
+		genererImage();
 		
 		bouton.setOnMouseEntered((e)->{
-			this.getChildren().add(imageView);
+			imageView.setStyle("-fx-opacity:100;");
+			
+		});
+		
+		bouton.setOnMouseExited((e)->{
+			imageView.setStyle("-fx-opacity:0;");
 			
 		});
 		}
+	
+		private void genererImage() {
+			J.appel(this);
+			InputStream streamImage = ImageAjustable.class.getResourceAsStream(url);
+			Image image = new Image(streamImage);
+			imageView = new ImageView(image);
+			imageView.setFitHeight(150);
+			imageView.setFitWidth(150);
+			imageView.setTranslateX(70);
+			this.getChildren().add(imageView);
+			imageView.setStyle("-fx-opacity:0;");
+			
+		}
+		
+		private void genererBouton(Button bouton, String type) {
+			J.appel(this);
+			this.setAlignment(Pos.BOTTOM_LEFT);
+			bouton.getStyleClass().add(type);
+			this.getChildren().add(bouton);
+			bouton.setMinWidth(80);
+			bouton.setMinHeight(40);
+		}
+		
 	
 
 
