9ae66de9ab6f324b49ce79acb7b95d9603d28f27
jfs39 <jfs.serg@hotmail.com>
2020-04-24 00:10:58 -0400

    Bonjour! J'ai tenté d'implémenter dans mon code une composante personnalisée de canevas ajustable afin de pouvoir simplifier le déplacement de mes images (J'ai réalisé qu'avec la façon dont je procédais cela serait difficile) sauf que le canevas ne semble rien afficher sur mon écran. J'ai énormément de difficultés avec cela je crois, est-ce que vous sauriez ce qui pose problème dans mon projet? (J'ai mis en commentaire toutes mes méthodes qui sont reliées au Canevas dans ma vue) Merci!

--- src/main/java/josser_combat_client/commandes/JeterCoup.java
+++ src/main/java/josser_combat_client/commandes/JeterCoup.java
@@ -5,18 +5,19 @@ import commun_client.commandes.Commande;
 public class JeterCoup extends Commande<JeterCoupPourEnvoi, JeterCoupRecue>   
 										implements JeterCoupPourEnvoi, 
 										JeterCoupRecue{
-	private int infoCoup;
-	
+
 	@Override
 	public int getInfoCoup() {
-
-		return infoCoup;
+		
+		return 0;
 	}
 
 	@Override
 	public void setInfoCoup(int infoCoup) {
+		
+		
+	
 
-		this.infoCoup = infoCoup;
 	}
 /*Ne sais pas trop encore, le type de coup jeté (le int) peux changer dans le futur TODO*/
 }
